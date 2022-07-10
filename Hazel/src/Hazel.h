943e191dcb0925d2b4c5c76bad6fb66c499b390d
ohmygollygosh68 <ohmygollygosh@outlook.com>
2022-07-10 20:56:22 +0100

    Entry  Point

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,5 @@
+#pragma once
+
+// For use by Hazel appplications
+
+#include "Hazel/Application.h"
---
+++ Sandbox/src/SandboxApp.cpp
@@ -0,0 +1,27 @@
+#include "Hazel.h"
+
+class Sandbox : public Hazel::Application
+{
+public:
+	Sandbox()
+	{
+
+	}
+
+
+	~Sandbox()
+	{
+
+	}
+
+
+};
+int main()
+{
+
+	Sandbox* sandbox = new Sandbox();
+	sandbox->Run();
+
+	delete sandbox;
+
+}
